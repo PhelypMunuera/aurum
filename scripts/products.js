9e0d70de60f9ae6da8b0be98d87555c076f90a90
Phelyp Munuera <mphelyp@gmail.com>
2026-01-06 20:56:24 -0300

    restructuring of the folders

---
+++ scripts/products.js
@@ -0,0 +1,11 @@
+
+export const products = [
+    {
+        id: crypto.randomUUID(),
+        image:'', 
+        tags:['esportivo'],
+        name:'' ,
+        descriptions:'',
+        price: 0, 
+    }
+]
