ddce231bbf82be8ba63890ca41a61eb3c6d3607f
Phelyp Munuera <mphelyp@gmail.com>
2026-01-06 21:38:16 -0300

    creat products

---
+++ scripts/fuction.js
@@ -0,0 +1,14 @@
+
+import { products } from "./products.js" 
+
+
+products.forEach((product) => {
+    console.log(product)
+})
+
+
+
+
+
+
+    // percorrer os produtos e renderizar um elemento html para cada produto 
