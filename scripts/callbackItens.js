1cf4c4e9260ead2a2dad761dd0bec7dbde9d2502
Phelyp Munuera <mphelyp@gmail.com>
2026-01-27 21:14:26 -0300

    refc: render with js

--- scripts/callbackItens.js
+++ scripts/callbackItens.js
@@ -6,15 +6,32 @@ export function printItens() {
   var item = {};
   function renderItens() {
     for (let i = 0; i < itensSave.length; i++) {
-      const backitem = products.find((p) => p.id === item.id);
       item = itensSave[i];
 
-      
-      console.log(backitem);
+      const backitem = products.find((p) => p.id === item.id);
 
-      //       unorderedList.innerHTML += `<li>
-      // <img src="../../assets/image/products/${products}" alt="" />
-      // </li>`;
+      console.log(backitem.image);
+
+      unorderedList.innerHTML += `<li>
+              <img src="../../assets/image/products/${backitem.image}.png" alt="" />
+              <div class="iten-select">
+                <h1>${backitem.name}</h1>
+                <div class="action-iten">
+                  <div class="amount">
+                    <button class=""><i class="ph ph-minus"></i></button>
+                    <input disabled type="number" value="${item.qtd}" />
+                    <button class=""><i class="ph ph-plus"></i></button>
+                  </div>
+                  <button class="remove">
+                    <i class="ph ph-trash"></i>
+                    Remover
+                  </button>
+                </div>
+              </div>
+              <span><strong>R$</strong> ${backitem.price * item.qtd}</span>
+
+
+      </li>`;
     }
   }
 
@@ -22,55 +39,3 @@ export function printItens() {
 }
 
 printItens();
-
-//       unorderedList.innerHTML += `<li>
-// <img src="../../assets/image/products/relogio-1.png" alt="" />
-
-//       <img
-//         src="./assets/image/products/${image}.png"
-//         alt="relogio 1"
-//       />
-
-//       <div class="tags">
-//          ${tags.map((tag) => `<span>${tag}</span>`).join("")}
-//       </div>
-
-//       <h3>${name}</h3>
-//       <p>${descriptions}</p>
-
-//       <footer>
-//         <span>R$<strong>${price}</strong></span>
-//         <div class="amount">
-//          <button class="btn-minus""> <i class="ph ph-minus"></i></button>
-//           <input disabled type="number" value="1"/>
-//           <button class="btn-plus"><i class="ph ph-plus"></i></button>
-//         </div>
-//         <button class="addToCard">
-//           <i class="ph-fill ph-shopping-cart"></i>
-//         </button>
-//       </footer>
-//     </article>
-
-//   <div class="iten-select">
-//     <h1>Expresso Tradicional</h1>
-//     <div class="action-iten">
-//       <div class="amount">
-//         <button class="">
-//           <i class="ph ph-minus"></i>
-//         </button>
-//         <input disabled type="number" value="1" />
-//         <button class="">
-//           <i class="ph ph-plus"></i>
-//         </button>
-//       </div>
-//       <button class="remove">
-//         <i class="ph ph-trash"></i>
-//         Remover
-//       </button>
-//     </div>
-//   </div>
-//   <span>
-//     <strong>R$</strong> 897,90
-//   </span>
-
-// </li>`;
